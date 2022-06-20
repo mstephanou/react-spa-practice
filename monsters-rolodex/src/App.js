ea9ba7b27fd30019b388eba4d24a205da6b43ac6
Michael Stephanou <mmstephanou@gmail.com>
2022-06-20 16:48:37 +0100

    refactoring from class components to functional components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,65 +1,61 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBox from './components/SearchBox/SearchBox';
 import CardList from './components/CardList/CardList';
 import './App.css';
 
-class App extends Component {
-  // 1 - constructor method for initialising state
-  constructor() {
-    super();
-    // Initial monsters value is an empty array
-    // searchField value is an empty string
-    //state should start witht he full list of monsters
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-    console.log('constructor');
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState(''); //[value, setValue]
+  const [monster, setMonsters] = useState([]);
 
-  componentDidMount() {
-    console.log('componentdidmount');
-    // 3 - componentdidmount method using promises to make API calls and mounts to DOM for rendering
-    // Once component mounts fetch the new users, call setState, then re-render
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
+  fetch('https://jsonplaceholder.typicode.com/users') // any fetch call is a 'side-effect' - therefore we need the useEffect hook
+    .then((res) => res.json())
+    .then((users) => setMonsters(users));
 
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
-  render() {
-    console.log('render');
-    // destructured objects in ES6 makes it easier to read and a performance improvement
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    // Filter takes a boolean, return value if monster name includes the search string in the search box
-    // It is not case sensititve so we need a new variable that converts searchField to lowercase
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
 
-    // 2
-    return (
-      <div className='App'>
-        <h1 className='app-title'>The Monster Squad</h1>
-        <SearchBox
-          className='monsters-search-box' // className prop == 'search-box' to be passed to other components
-          onChange={onSearchChange} // passing props to other components from here | onChange prop = {onSearchChange} event handler
-          placeholder='search monsters' // passing props to other components from here | placeholder prop = 'search monsters'
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  const filteredMonsters = this.state.monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>The Monster Squad</h1>
+      <SearchBox
+        className='monsters-search-box' // className prop == 'search-box' to be passed to other components
+        onChange={onSearchChange} // passing props to other components from here | onChange prop = {onSearchChange} event handler
+        placeholder='search monsters' // passing props to other components from here | placeholder prop = 'search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+
+//     // 3 - componentdidmount method using promises to make API calls and mounts to DOM for rendering
+//     // Once component mounts fetch the new users, call setState, then re-render
+
+//       .then((res) => res.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+
+//     // destructured objects in ES6 makes it easier to read and a performance improvement
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     // Filter takes a boolean, return value if monster name includes the search string in the search box
+//     // It is not case sensititve so we need a new variable that converts searchField to lowercase
+//     );
+
+// }
 
 export default App;
